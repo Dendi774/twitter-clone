74d9ba748f3be7fe02b46397c23057c35dbb0b6c
Olivar <denmarkoh29@gmail.com>
2026-08-24 19:47:19 +0800

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@
 import express, { urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
 import {v2 as cloudinary} from "cloudinary";
+import path from 'path';
 
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
@@ -22,20 +23,25 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({extended: true})) // to parse form data(urlencoded)
 
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the twitter clone API');
-})
-
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationRoutes);
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*splat", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     connectDB();
